de21fa1c9bf25ea1c96f93a756b0faf5ac5dc8ec
stvik <stvik2@gmail.com>
2020-01-10 17:19:53 -0500

    rendered some sushis

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,36 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+	constructor() {
+		super()
+		this.state = {
+			allSushis: [],
+			eatenSushis: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(API)
+		.then(resp => resp.json())
+		.then(data => {
+			this.setState({
+				allSushis: data
+				})
+			
+		})
+	}
+
+	eatSushi = (sushi) => {
+		this.setState({
+			eatenSushi: [...this.state.eatSushi,sushi]
+		})
+	}
+
   render() {
+  	console.log(this.state)
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={this.state.allSushis.slice(0,4)} eatSushi={this.eatSushi} eatenSushi={this.state.eatenSushi}/>
         <Table />
       </div>
     );
